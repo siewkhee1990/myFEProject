0fa34405ee895ee60f744a7fa882bb34df1fa0f7
siewkhee1990 <siewkhee1990@hotmail.com>
2024-03-30 02:11:02 +0800

    changed router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import SingpassLogin from "./SingpassLogin";
+import ProcessToken from "./ProcessToken";
+import TokenAuth from "./TokenAuth";
+import PostLogin from "./PostLogin";
+import NotFound from "./NotFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<SingpassLogin />} />
+        <Route path="/token" element={<ProcessToken />} />
+        <Route path="/tokenAuth" element={<TokenAuth />} />
+        <Route path="/postLogin" element={<PostLogin />} />
+        <Route element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
